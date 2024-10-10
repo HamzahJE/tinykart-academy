f6814b6a6aa6b446064b299106eb8de86ea9c936
Hamzah Jamal Eddin <hamzahjeddin@gmail.com>
2024-10-10 17:50:16 -0400

    move_motor

--- src/main.cpp
+++ src/main.cpp
@@ -15,17 +15,19 @@ TinyKart *tinyKart;
 LD06 ld06{};
 
 // Scan processor
-ScanBuilder scan_builder{180,360, ScanPoint{0,0}};
+ScanBuilder scan_builder{180, 360, ScanPoint{0,0}};
 
 /// Starts/stops the kart
-void estop() {
+void estop()
+{
     logger.printf("Toggle Pause\n");
 
     tinyKart->toggle_pause();
     digitalToggle(LED_YELLOW);
 }
 
-void setup() {
+void setup()
+{
     // LEDs
     pinMode(LED_GREEN, OUTPUT);
     pinMode(LED_RED, OUTPUT);
@@ -47,78 +49,74 @@ void setup() {
     tinyKart = new TinyKart{STEERING_PIN, esc, 0.3, 4.5};
 
     // Init DMA and UART for LiDAR
-    dmaSerialRx5.begin(230'400, [&](volatile LD06Buffer buffer) {
+    dmaSerialRx5.begin(230'400, [&](volatile LD06Buffer buffer)
+                       {
         // On each packet received, copy over to driver.
-        ld06.add_buffer(buffer, 47);
-    });
+        ld06.add_buffer(buffer, 47); });
 
     digitalWrite(LED_RED, LOW);
     digitalWrite(LED_GREEN, HIGH);
 }
 
-void loop() {
+void loop()
+{
     noInterrupts();
     auto res = ld06.get_scan();
     interrupts();
 
     // Check if we have a scan frame
-    if (res) {
+    if (res)
+    {
         auto scan_res = *res;
 
         // Check if frame erred
-        if (scan_res) {
+        if (scan_res)
+        {
             auto maybe_scan = scan_builder.add_frame(scan_res.scan);
 
             // Check if we have a 180 degree scan built
-            if (maybe_scan) {
+            if (maybe_scan)
+            {
                 auto scan = *maybe_scan;
-                // auto third_scan = scan[scan.size()/3];
-                // auto two_third_scan = scan[2*scan.size()/3];
-                // auto first = scan[0];
-                // auto last = scan[scan.size()-1];
-
                 logger.printf("*****START SCAN******\n");
-                int count=0;
-                int nxcount=0;
-                int nycount=0;
+                bool canMove = true;
 
-        for (auto &pt: scan) {
-                pt.y=pt.y*100;
-                pt.x=pt.x*100;
+                for (auto &pt : scan)
+                {
+                    pt.x*=100;
+                    pt.y*=100;
 
-    if (pt.x == 0 && pt.y == 0) {count++; continue;}
-  
-     if( pt.x < 0){
-        nxcount++;
-     }
-     if( pt.y <0){
-        nycount++;
-     }
-  
-
-
-      if((pt.x> -10 && pt.x < 10 ) && (pt.y > 0 && pt.y < 27)){
-
+                    if ((pt.x > -10 && pt.x < 10) && (pt.y > 0 && pt.y < 140))
+                    {
                         logger.printf("\nIn box");
-
-
-   }
-
+                        canMove = false;
+                        break;
+                    }
                 }
-                logger.printf("\ncount: %d, scan_size: %d, negativeX: %d,negativeY: %d",count,scan.size(), nxcount, nycount);
 
+                if (canMove)
+                {
+                    tinyKart->set_steering(0);
+                    tinyKart->set_forward(0.2);
+                }
+                else
+                {
+                    tinyKart->set_neutral();
+                }
                 logger.printf("*****END SCAN******\n\n");
             }
+        }
+        else
+        {
+            switch (scan_res.error)
+            {
+            case ScanResult::Error::CRCFail:
+                logger.printf("CRC error!\n");
+                break;
 
-        } else {
-            switch (scan_res.error) {
-                case ScanResult::Error::CRCFail:
-                    logger.printf("CRC error!\n");
-                    break;
-
-                case ScanResult::Error::HeaderByteWrong:
-                    logger.printf("Header byte wrong!\n");
-                    break;
+            case ScanResult::Error::HeaderByteWrong:
+                logger.printf("Header byte wrong!\n");
+                break;
             }
         }
     }
